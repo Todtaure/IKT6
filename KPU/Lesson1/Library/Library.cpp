12c920719ec1e317c90674d5151717000473f523
Todtaure <razak.todtaure@gmail.com>
2015-02-03 21:23:20 +0100

    KPU: Lab01 continued

--- KPU/Lesson1/Library/Library.cpp
+++ KPU/Lesson1/Library/Library.cpp
@@ -5,22 +5,6 @@
 #include "Library.h"
 
 
-// This is an example of an exported variable
-LIBRARY_API int nLibrary=0;
-
-// This is an example of an exported function.
-LIBRARY_API int fnLibrary(void)
-{
-	return 42;
-}
-
-// This is the constructor of a class that has been exported.
-// see Library.h for the class definition
-CLibrary::CLibrary()
-{
-	return;
-}
-
 int sumInt32(int first, int second)
 {
 	return first += second;
@@ -31,13 +15,12 @@ std::string concatString(std::string first, std::string second)
 	return first += second;
 }
 
-char* concatChar(char first, char second)
+char* concatChar(char* first, char* second)
 {
-	char tmp[3];
-
-	tmp[0] = first;
-	tmp[1] = second;
-	tmp[2] = '\0';
-
-	return tmp;
+	size_t len = strlen(first) + strlen(second) + 1;
+	char* retVal = new char[len];
+	retVal[0] = 0;  // Empty string
+	strcat_s(retVal, len, first);
+	strcat_s(retVal, len, second);
+	return retVal;
 }
