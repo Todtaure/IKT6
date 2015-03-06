3717187d7bf92900716c9fb2062233cf81179df3
CSDahl <christiansdahl@gmail.com>
2015-03-06 11:42:40 +0100

    Lesson12 KPU PCL

--- KPU/ATLMathServer/ATLMathServer_i.h
+++ KPU/ATLMathServer/ATLMathServer_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Tue Mar 03 13:09:15 2015
+/* at Fri Mar 06 09:19:25 2015
  */
 /* Compiler settings for ATLMathServer.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
---
+++ KPU/MyTwitterSample/TwitterApp.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml.Linq;
+
+namespace MyTwitterSample
+{
+    public class TwitterApp
+    {
+        public void Verify_Credentials()
+        {
+            string oauthconsumerkey = "your consumerkey";
+            string oauthconsumersecret = "your consumer secret key";
+            string oauthsignaturemethod = "HMAC-SHA1";
+            string oauthversion = "1.0";
+            string oauthtoken = "your oauth token";
+            string oauthtokensecret = "your oauth token secret";
+            string oauthnonce = Convert.ToBase64String(new ASCIIEncoding().GetBytes(DateTime.Now.Ticks.ToString()));
+            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            string oauthtimestamp = Convert.ToInt64(ts.TotalSeconds).ToString();
+            SortedDictionary<string, string> basestringParameters = new SortedDictionary<string, string>();
+            basestringParameters.Add("oauth_version", "1.0");
+            basestringParameters.Add("oauth_consumer_key", oauthconsumerkey);
+            basestringParameters.Add("oauth_nonce", oauthnonce);
+            basestringParameters.Add("oauth_signature_method", "HMAC-SHA1");
+            basestringParameters.Add("oauth_timestamp", oauthtimestamp);
+            basestringParameters.Add("oauth_token", oauthtoken);
+            //GS - Build the signature string
+            StringBuilder baseString = new StringBuilder();
+            baseString.Append("GET" + "&");
+            baseString.Append(EncodeCharacters(Uri.EscapeDataString("https://api.twitter.com/1.1/account/verify_credentials.json") + "&"));
+            foreach (KeyValuePair<string, string> entry in basestringParameters)
+            {
+                baseString.Append(EncodeCharacters(Uri.EscapeDataString(entry.Key + "=" + entry.Value + "&")));
+            }
+
+            //Since the baseString is urlEncoded we have to remove the last 3 chars - %26
+            string finalBaseString = baseString.ToString().Substring(0, baseString.Length - 3);
+
+            //Build the signing key
+            string signingKey = EncodeCharacters(Uri.EscapeDataString(oauthconsumersecret)) + "&" +
+            EncodeCharacters(Uri.EscapeDataString(oauthtokensecret));
+
+            //Sign the request
+            HMACSHA1 hasher = new HMACSHA1(new ASCIIEncoding().GetBytes(signingKey));
+            string oauthsignature = Convert.ToBase64String(hasher.ComputeHash(new ASCIIEncoding().GetBytes(finalBaseString)));
+
+            //Tell Twitter we don't do the 100 continue thing
+            ServicePointManager.Expect100Continue = false;
+
+            //authorization header
+            HttpWebRequest hwr = (HttpWebRequest)WebRequest.Create(
+              @"https://api.twitter.com/1.1/account/verify_credentials.json");
+            StringBuilder authorizationHeaderParams = new StringBuilder();
+            authorizationHeaderParams.Append("OAuth ");
+            authorizationHeaderParams.Append("oauth_nonce=" + "\"" + Uri.EscapeDataString(oauthnonce) + "\",");
+            authorizationHeaderParams.Append("oauth_signature_method=" + "\"" + Uri.EscapeDataString(oauthsignaturemethod) + "\",");
+            authorizationHeaderParams.Append("oauth_timestamp=" + "\"" + Uri.EscapeDataString(oauthtimestamp) + "\",");
+            authorizationHeaderParams.Append("oauth_consumer_key=" + "\"" + Uri.EscapeDataString(oauthconsumerkey) + "\",");
+            if (!string.IsNullOrEmpty(oauthtoken))
+                authorizationHeaderParams.Append("oauth_token=" + "\"" + Uri.EscapeDataString(oauthtoken) + "\",");
+            authorizationHeaderParams.Append("oauth_signature=" + "\"" + Uri.EscapeDataString(oauthsignature) + "\",");
+            authorizationHeaderParams.Append("oauth_version=" + "\"" + Uri.EscapeDataString(oauthversion) + "\"");
+            hwr.Headers.Add("Authorization", authorizationHeaderParams);
+            hwr.Method = "GET";
+            hwr.ContentType = "application/x-www-form-urlencoded";
+
+            //Allow us a reasonable timeout in case Twitter's busy
+            hwr.Timeout = 3 * 60 * 1000;
+            try
+            {
+                hwr.Proxy = new WebProxy("enter proxy details/address");
+                HttpWebResponse rsp = hwr.GetResponse() as HttpWebResponse;
+                Stream dataStream = rsp.GetResponseStream();
+                //Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                //Read the content.
+                string responseFromServer = reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+       
+        
+    }
+}
