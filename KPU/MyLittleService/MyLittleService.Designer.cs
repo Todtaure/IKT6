7ee3484da3f4f110e8af255d16af4f00785b2fe2
CSDahl <christiansdahl@gmail.com>
2015-03-13 11:54:41 +0100

    Lesson14 KPU windows service buggy edition

---
+++ KPU/MyLittleService/MyLittleService.Designer.cs
@@ -0,0 +1,41 @@
+﻿namespace MyLittleService
+{
+    partial class MyLittleService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+        private System.Diagnostics.EventLog eventLog1;
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // 
+            // MyLittleService
+            // 
+            this.AutoLog = false;
+            this.ServiceName = "MyLittleService";
+
+        }
+
+        #endregion
+    }
+}
---
+++ KPU/MyLittleService/MyLittleService.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace MyLittleService
+{
+    public class FileWatcher
+    {
+        private string _sourcePath;
+        private string _targetPath;
+
+        private delegate void WatcherDelegate(string sourcePath);
+
+        private WatcherDelegate Wd;
+        public FileWatcher(string sourcePath, string targetPath)
+        {
+            _sourcePath = sourcePath;
+            _targetPath = targetPath;
+            Wd = new WatcherDelegate(TurnOnWatch);
+
+            if (!Directory.Exists(_sourcePath) || !Directory.Exists(_targetPath))
+            {
+                return;
+            }
+        }
+
+        public void StartToWatch()
+        {
+            Wd.BeginInvoke(_sourcePath, null,null);
+        }
+        private void TurnOnWatch(string sourcePath)
+        {
+            var watcher = new FileSystemWatcher(sourcePath);
+
+            watcher.Changed += new FileSystemEventHandler(EventHandler);
+        }
+
+        public void EventHandler(object source, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+            FileStream fsSource = new FileStream(e.FullPath, FileMode.Open, FileAccess.Read);
+            FileStream fsTarget = new FileStream(_targetPath, FileMode.Append, FileAccess.ReadWrite);
+
+            try
+            {
+                fsSource.CopyTo(fsTarget);
+
+            }
+            finally
+            {
+                fsTarget.Close();
+                fsSource.Close();
+            }
+        }
+    }
+
+    public partial class MyLittleService : ServiceBase
+    {
+        public MyLittleService()
+        {
+            this.AutoLog = false;
+            InitializeComponent();
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "MySource", "MyNewLog");
+            }
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart");
+            FileWatcher fileWatcher = new FileWatcher(@"C:\tmp", @"C:\targetTmp\tmp.txt");
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In onStop.");
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("In OnContinue.");
+        }
+    }
+}
